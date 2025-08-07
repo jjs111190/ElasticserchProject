6e111633b711b95a968465a257934cd364c7e6c8
MAC(Jae Seok) <jaeseok@MACJaeui-MacBookPro.local>
2025-08-07 09:47:26 +0900

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ElasticscerchProject/',
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
